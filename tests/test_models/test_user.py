0d1bf0ca5434156bf09a5051cb8acfd53d9d9f5a
abraham <wole1702@gmail.com>
2024-03-25 01:42:13 +0100

    tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -23,3 +23,14 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(user, 'password'))
         self.assertTrue(hasattr(user, 'first_name'))
         self.assertTrue(hasattr(user, 'last_name'))
+
+    def test_class_attributes(self):
+        # Test if class attributes are initialized correctly
+        self.assertEqual(User.email, "")
+        self.assertEqual(User.password, "")
+        self.assertEqual(User.first_name, "")
+        self.assertEqual(User.last_name, "")
+
+    def test_inheritance(self):
+        # Test if User class inherits from BaseModel
+        self.assertTrue(issubclass(User, BaseModel))
